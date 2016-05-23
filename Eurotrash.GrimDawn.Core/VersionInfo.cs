d4bd177d2b30db1b626788c0646d81f04f9f2dfe
Daniel Barla-Szabo <danielbarla@gmail.com>
2016-05-23 10:16:06 +0200

    Version 0.2

--- Eurotrash.GrimDawn.Core/VersionInfo.cs
+++ Eurotrash.GrimDawn.Core/VersionInfo.cs
@@ -4,8 +4,8 @@ namespace Eurotrash.GrimDawn.Core
 {
     public static class VersionInfo
     {
-        public const string ShortVersion = "0.1";
+        public const string ShortVersion = "0.2";
 
-        public const string FullVersion = "0.1.0.0";
+        public const string FullVersion = "0.2.0.0";
     }
 }
